a6a9452162fea4cc0c0932fa42bf03c913e7ba08
Rob <rob.rimmer@mclaren.com>
2020-12-17 14:37:44 +0000

    Refactor of map key Tests

--- box_drawing.go
+++ box_drawing.go
@@ -29,16 +29,23 @@ func (bt BoxType) String() string {
 	panic("Unknown box type")
 }
 
+type BoxParts struct {
+	Up    BoxType
+	Down  BoxType
+	Left  BoxType
+	Right BoxType
+}
+
 func GetBoxChar(up bool, down bool, left bool, right bool, boxType BoxType) (char rune, ok bool) {
 	upBt := ConditionalBoxType(up, boxType, BoxNone)
 	downBt := ConditionalBoxType(down, boxType, BoxNone)
 	leftBt := ConditionalBoxType(left, boxType, BoxNone)
 	rightBt := ConditionalBoxType(right, boxType, BoxNone)
-	return GetBoxCharMixed(upBt, downBt, leftBt, rightBt)
+	return GetBoxCharMixed(BoxParts{upBt, downBt, leftBt, rightBt})
 }
 
-func GetBoxCharMixed(up BoxType, down BoxType, left BoxType, right BoxType) (char rune, ok bool) {
-	char, ok = boxParts[createDir(up, down, left, right)]
+func GetBoxCharMixed(parts BoxParts) (char rune, ok bool) {
+	char, ok = boxParts[parts]
 	return char, ok
 }
 
@@ -47,40 +54,40 @@ func MustGetBoxChar(up bool, down bool, left bool, right bool, boxType BoxType) 
 	downBt := ConditionalBoxType(down, boxType, BoxNone)
 	leftBt := ConditionalBoxType(left, boxType, BoxNone)
 	rightBt := ConditionalBoxType(right, boxType, BoxNone)
-	return MustGetBoxCharMixed(upBt, downBt, leftBt, rightBt)
+	return MustGetBoxCharMixed(BoxParts{upBt, downBt, leftBt, rightBt})
 }
 
-func MustGetBoxCharMixed(up BoxType, down BoxType, left BoxType, right BoxType) rune {
-	if char, ok := GetBoxCharMixed(up, down, left, right); ok {
+func MustGetBoxCharMixed(parts BoxParts) rune {
+	if char, ok := GetBoxCharMixed(parts); ok {
 		return char
 	}
 	msg := strings.Builder{}
 	msg.WriteString("Box drawing character not found: ")
 	sep := ""
-	if up != BoxNone {
-		msg.WriteString(fmt.Sprintf("%sup=%s", sep, up))
+	if parts.Up != BoxNone {
+		msg.WriteString(fmt.Sprintf("%sup=%s", sep, parts.Up))
 		sep = ", "
 	}
-	if down != BoxNone {
-		msg.WriteString(fmt.Sprintf("%sdown=%s", sep, down))
+	if parts.Down != BoxNone {
+		msg.WriteString(fmt.Sprintf("%sparts.Down=%s", sep, parts.Down))
 		sep = ", "
 	}
-	if left != BoxNone {
-		msg.WriteString(fmt.Sprintf("%sleft=%s", sep, left))
+	if parts.Left != BoxNone {
+		msg.WriteString(fmt.Sprintf("%sparts.Left=%s", sep, parts.Left))
 		sep = ", "
 	}
-	if right != BoxNone {
-		msg.WriteString(fmt.Sprintf("%sright=%s", sep, right))
+	if parts.Right != BoxNone {
+		msg.WriteString(fmt.Sprintf("%sparts.Right=%s", sep, parts.Right))
 	}
 	panic(msg.String())
 }
 
 func GetHorizontal(bt BoxType) rune {
-	return MustGetBoxChar(false,false, true,true, bt)
+	return MustGetBoxChar(false, false, true, true, bt)
 }
 
 func GetVertical(bt BoxType) rune {
-	return MustGetBoxChar( true,true,false,false, bt)
+	return MustGetBoxChar(true, true, false, false, bt)
 }
 
 func ConditionalBoxType(c bool, t BoxType, f BoxType) BoxType {
@@ -95,166 +102,131 @@ func (bt BoxType) HeavyIf(c bool) BoxType {
 	return ConditionalBoxType(c, BoxHeavy, bt)
 }
 
-type direction uint32
+// ----------------------------------------------------------------------------------------------------------------------------
+// Implementation
+// ----------------------------------------------------------------------------------------------------------------------------
 
-func (d direction) getUp() BoxType {
-	return d.getDir(0xff000000, 24)
-}
-func (d *direction) setUp(boxType BoxType) *direction {
-	return d.setDir(0x00ffffff, 24, boxType)
-}
-func (d direction) getDown() BoxType {
-	return d.getDir(0x00ff0000, 16)
-}
-func (d *direction) setDown(boxType BoxType) *direction {
-	return d.setDir(0xff00ffff, 16, boxType)
-}
-func (d direction) getLeft() BoxType {
-	return d.getDir(0x0000ff00, 8)
-}
-func (d *direction) setLeft(boxType BoxType) *direction {
-	return d.setDir(0xffff00ff, 8, boxType)
-}
-func (d direction) getRight() BoxType {
-	return d.getDir(0x000000ff, 0)
-}
-func (d *direction) setRight(boxType BoxType) *direction {
-	return d.setDir(0xffffff00, 0, boxType)
-}
-func (d direction) getDir(mask uint32, shift int) BoxType {
-	return BoxType((uint32(d) & mask) >> shift)
-}
-func (d *direction) setDir(mask uint32, shift int, boxType BoxType) *direction {
-	*d = *d & direction(mask)
-	*d = *d | direction(boxType<<shift)
-	return d
-}
-func createDir(up BoxType, down BoxType, left BoxType, right BoxType) direction {
-	d := direction(0)
-	return *d.setUp(up).setDown(down).setLeft(left).setRight(right)
-}
-
-var boxParts = map[direction]rune{
+var boxParts = map[BoxParts]rune{
 	// Space
-	createDir(BoxNone, BoxNone, BoxNone, BoxNone): ' ',
+	BoxParts{BoxNone, BoxNone, BoxNone, BoxNone}: ' ',
 	// Half lines
-	createDir(BoxSingle, BoxNone, BoxNone, BoxNone): '╵',
-	createDir(BoxNone, BoxSingle, BoxNone, BoxNone): '╷',
-	createDir(BoxNone, BoxNone, BoxSingle, BoxNone): '╴',
-	createDir(BoxNone, BoxNone, BoxNone, BoxSingle): '╶',
-	createDir(BoxHeavy, BoxNone, BoxNone, BoxNone):  '╹',
-	createDir(BoxNone, BoxHeavy, BoxNone, BoxNone):  '╻',
-	createDir(BoxNone, BoxNone, BoxHeavy, BoxNone):  '╸',
-	createDir(BoxNone, BoxNone, BoxNone, BoxHeavy):  '╺',
+	BoxParts{BoxSingle, BoxNone, BoxNone, BoxNone}: '╵',
+	BoxParts{BoxNone, BoxSingle, BoxNone, BoxNone}: '╷',
+	BoxParts{BoxNone, BoxNone, BoxSingle, BoxNone}: '╴',
+	BoxParts{BoxNone, BoxNone, BoxNone, BoxSingle}: '╶',
+	BoxParts{BoxHeavy, BoxNone, BoxNone, BoxNone}:  '╹',
+	BoxParts{BoxNone, BoxHeavy, BoxNone, BoxNone}:  '╻',
+	BoxParts{BoxNone, BoxNone, BoxHeavy, BoxNone}:  '╸',
+	BoxParts{BoxNone, BoxNone, BoxNone, BoxHeavy}:  '╺',
 	// Full lines
-	createDir(BoxSingle, BoxSingle, BoxNone, BoxNone): '│',
-	createDir(BoxNone, BoxNone, BoxSingle, BoxSingle): '─',
-	createDir(BoxDouble, BoxDouble, BoxNone, BoxNone): '║',
-	createDir(BoxNone, BoxNone, BoxDouble, BoxDouble): '═',
-	createDir(BoxHeavy, BoxHeavy, BoxNone, BoxNone):   '┃',
-	createDir(BoxNone, BoxNone, BoxHeavy, BoxHeavy):   '━',
-	createDir(BoxHeavy, BoxSingle, BoxNone, BoxNone):  '╿',
-	createDir(BoxSingle, BoxHeavy, BoxNone, BoxNone):  '╽',
-	createDir(BoxNone, BoxNone, BoxHeavy, BoxSingle):  '╾',
-	createDir(BoxNone, BoxNone, BoxSingle, BoxHeavy):  '╼',
+	BoxParts{BoxSingle, BoxSingle, BoxNone, BoxNone}: '│',
+	BoxParts{BoxNone, BoxNone, BoxSingle, BoxSingle}: '─',
+	BoxParts{BoxDouble, BoxDouble, BoxNone, BoxNone}: '║',
+	BoxParts{BoxNone, BoxNone, BoxDouble, BoxDouble}: '═',
+	BoxParts{BoxHeavy, BoxHeavy, BoxNone, BoxNone}:   '┃',
+	BoxParts{BoxNone, BoxNone, BoxHeavy, BoxHeavy}:   '━',
+	BoxParts{BoxHeavy, BoxSingle, BoxNone, BoxNone}:  '╿',
+	BoxParts{BoxSingle, BoxHeavy, BoxNone, BoxNone}:  '╽',
+	BoxParts{BoxNone, BoxNone, BoxHeavy, BoxSingle}:  '╾',
+	BoxParts{BoxNone, BoxNone, BoxSingle, BoxHeavy}:  '╼',
 	// Up-Left
-	createDir(BoxSingle, BoxNone, BoxSingle, BoxNone): '┘',
-	createDir(BoxDouble, BoxNone, BoxDouble, BoxNone): '╝',
-	createDir(BoxDouble, BoxNone, BoxSingle, BoxNone): '╜',
-	createDir(BoxSingle, BoxNone, BoxDouble, BoxNone): '╛',
-	createDir(BoxHeavy, BoxNone, BoxHeavy, BoxNone):   '┛',
-	createDir(BoxHeavy, BoxNone, BoxSingle, BoxNone):  '┚',
-	createDir(BoxSingle, BoxNone, BoxHeavy, BoxNone):  '┙',
+	BoxParts{BoxSingle, BoxNone, BoxSingle, BoxNone}: '┘',
+	BoxParts{BoxDouble, BoxNone, BoxDouble, BoxNone}: '╝',
+	BoxParts{BoxDouble, BoxNone, BoxSingle, BoxNone}: '╜',
+	BoxParts{BoxSingle, BoxNone, BoxDouble, BoxNone}: '╛',
+	BoxParts{BoxHeavy, BoxNone, BoxHeavy, BoxNone}:   '┛',
+	BoxParts{BoxHeavy, BoxNone, BoxSingle, BoxNone}:  '┚',
+	BoxParts{BoxSingle, BoxNone, BoxHeavy, BoxNone}:  '┙',
 	// Up-Right
-	createDir(BoxSingle, BoxNone, BoxNone, BoxSingle): '└',
-	createDir(BoxDouble, BoxNone, BoxNone, BoxDouble): '╚',
-	createDir(BoxDouble, BoxNone, BoxNone, BoxSingle): '╙',
-	createDir(BoxSingle, BoxNone, BoxNone, BoxDouble): '╘',
-	createDir(BoxHeavy, BoxNone, BoxNone, BoxHeavy):   '┗',
-	createDir(BoxHeavy, BoxNone, BoxNone, BoxSingle):  '┖',
-	createDir(BoxSingle, BoxNone, BoxNone, BoxHeavy):  '┕',
+	BoxParts{BoxSingle, BoxNone, BoxNone, BoxSingle}: '└',
+	BoxParts{BoxDouble, BoxNone, BoxNone, BoxDouble}: '╚',
+	BoxParts{BoxDouble, BoxNone, BoxNone, BoxSingle}: '╙',
+	BoxParts{BoxSingle, BoxNone, BoxNone, BoxDouble}: '╘',
+	BoxParts{BoxHeavy, BoxNone, BoxNone, BoxHeavy}:   '┗',
+	BoxParts{BoxHeavy, BoxNone, BoxNone, BoxSingle}:  '┖',
+	BoxParts{BoxSingle, BoxNone, BoxNone, BoxHeavy}:  '┕',
 	// Down-Left
-	createDir(BoxNone, BoxSingle, BoxSingle, BoxNone): '┐',
-	createDir(BoxNone, BoxDouble, BoxDouble, BoxNone): '╗',
-	createDir(BoxNone, BoxDouble, BoxSingle, BoxNone): '╖',
-	createDir(BoxNone, BoxSingle, BoxDouble, BoxNone): '╕',
-	createDir(BoxNone, BoxHeavy, BoxHeavy, BoxNone):   '┓',
-	createDir(BoxNone, BoxHeavy, BoxSingle, BoxNone):  '┒',
-	createDir(BoxNone, BoxSingle, BoxHeavy, BoxNone):  '┑',
+	BoxParts{BoxNone, BoxSingle, BoxSingle, BoxNone}: '┐',
+	BoxParts{BoxNone, BoxDouble, BoxDouble, BoxNone}: '╗',
+	BoxParts{BoxNone, BoxDouble, BoxSingle, BoxNone}: '╖',
+	BoxParts{BoxNone, BoxSingle, BoxDouble, BoxNone}: '╕',
+	BoxParts{BoxNone, BoxHeavy, BoxHeavy, BoxNone}:   '┓',
+	BoxParts{BoxNone, BoxHeavy, BoxSingle, BoxNone}:  '┒',
+	BoxParts{BoxNone, BoxSingle, BoxHeavy, BoxNone}:  '┑',
 	// Down-Right
-	createDir(BoxNone, BoxSingle, BoxNone, BoxSingle): '┌',
-	createDir(BoxNone, BoxDouble, BoxNone, BoxDouble): '╔',
-	createDir(BoxNone, BoxDouble, BoxNone, BoxSingle): '╓',
-	createDir(BoxNone, BoxSingle, BoxNone, BoxDouble): '╒',
-	createDir(BoxNone, BoxHeavy, BoxNone, BoxHeavy):   '┏',
-	createDir(BoxNone, BoxHeavy, BoxNone, BoxSingle):  '┎',
-	createDir(BoxNone, BoxSingle, BoxNone, BoxHeavy):  '┍',
+	BoxParts{BoxNone, BoxSingle, BoxNone, BoxSingle}: '┌',
+	BoxParts{BoxNone, BoxDouble, BoxNone, BoxDouble}: '╔',
+	BoxParts{BoxNone, BoxDouble, BoxNone, BoxSingle}: '╓',
+	BoxParts{BoxNone, BoxSingle, BoxNone, BoxDouble}: '╒',
+	BoxParts{BoxNone, BoxHeavy, BoxNone, BoxHeavy}:   '┏',
+	BoxParts{BoxNone, BoxHeavy, BoxNone, BoxSingle}:  '┎',
+	BoxParts{BoxNone, BoxSingle, BoxNone, BoxHeavy}:  '┍',
 	// T-Up
-	createDir(BoxSingle, BoxNone, BoxSingle, BoxSingle): '┴',
-	createDir(BoxDouble, BoxNone, BoxDouble, BoxDouble): '╩',
-	createDir(BoxSingle, BoxNone, BoxDouble, BoxDouble): '╧',
-	createDir(BoxDouble, BoxNone, BoxSingle, BoxSingle): '╨',
-	createDir(BoxHeavy, BoxNone, BoxHeavy, BoxHeavy):    '┻',
-	createDir(BoxSingle, BoxNone, BoxHeavy, BoxHeavy):   '┷',
-	createDir(BoxHeavy, BoxNone, BoxSingle, BoxSingle):  '┸',
-	createDir(BoxHeavy, BoxNone, BoxSingle, BoxHeavy):   '┺',
-	createDir(BoxSingle, BoxNone, BoxHeavy, BoxSingle):  '┵',
-	createDir(BoxHeavy, BoxNone, BoxHeavy, BoxSingle):   '┹',
-	createDir(BoxSingle, BoxNone, BoxSingle, BoxHeavy):  '┶',
+	BoxParts{BoxSingle, BoxNone, BoxSingle, BoxSingle}: '┴',
+	BoxParts{BoxDouble, BoxNone, BoxDouble, BoxDouble}: '╩',
+	BoxParts{BoxSingle, BoxNone, BoxDouble, BoxDouble}: '╧',
+	BoxParts{BoxDouble, BoxNone, BoxSingle, BoxSingle}: '╨',
+	BoxParts{BoxHeavy, BoxNone, BoxHeavy, BoxHeavy}:    '┻',
+	BoxParts{BoxSingle, BoxNone, BoxHeavy, BoxHeavy}:   '┷',
+	BoxParts{BoxHeavy, BoxNone, BoxSingle, BoxSingle}:  '┸',
+	BoxParts{BoxHeavy, BoxNone, BoxSingle, BoxHeavy}:   '┺',
+	BoxParts{BoxSingle, BoxNone, BoxHeavy, BoxSingle}:  '┵',
+	BoxParts{BoxHeavy, BoxNone, BoxHeavy, BoxSingle}:   '┹',
+	BoxParts{BoxSingle, BoxNone, BoxSingle, BoxHeavy}:  '┶',
 	// T-Down
-	createDir(BoxNone, BoxSingle, BoxSingle, BoxSingle): '┬',
-	createDir(BoxNone, BoxDouble, BoxDouble, BoxDouble): '╦',
-	createDir(BoxNone, BoxSingle, BoxDouble, BoxDouble): '╤',
-	createDir(BoxNone, BoxDouble, BoxSingle, BoxSingle): '╥',
-	createDir(BoxNone, BoxHeavy, BoxHeavy, BoxHeavy):    '┳',
-	createDir(BoxNone, BoxSingle, BoxHeavy, BoxHeavy):   '┯',
-	createDir(BoxNone, BoxHeavy, BoxSingle, BoxSingle):  '┰',
-	createDir(BoxNone, BoxHeavy, BoxSingle, BoxHeavy):   '┲',
-	createDir(BoxNone, BoxSingle, BoxHeavy, BoxSingle):  '┭',
-	createDir(BoxNone, BoxHeavy, BoxHeavy, BoxSingle):   '┱',
-	createDir(BoxNone, BoxSingle, BoxSingle, BoxHeavy):  '┮',
+	BoxParts{BoxNone, BoxSingle, BoxSingle, BoxSingle}: '┬',
+	BoxParts{BoxNone, BoxDouble, BoxDouble, BoxDouble}: '╦',
+	BoxParts{BoxNone, BoxSingle, BoxDouble, BoxDouble}: '╤',
+	BoxParts{BoxNone, BoxDouble, BoxSingle, BoxSingle}: '╥',
+	BoxParts{BoxNone, BoxHeavy, BoxHeavy, BoxHeavy}:    '┳',
+	BoxParts{BoxNone, BoxSingle, BoxHeavy, BoxHeavy}:   '┯',
+	BoxParts{BoxNone, BoxHeavy, BoxSingle, BoxSingle}:  '┰',
+	BoxParts{BoxNone, BoxHeavy, BoxSingle, BoxHeavy}:   '┲',
+	BoxParts{BoxNone, BoxSingle, BoxHeavy, BoxSingle}:  '┭',
+	BoxParts{BoxNone, BoxHeavy, BoxHeavy, BoxSingle}:   '┱',
+	BoxParts{BoxNone, BoxSingle, BoxSingle, BoxHeavy}:  '┮',
 	// T-Left
-	createDir(BoxSingle, BoxSingle, BoxSingle, BoxNone): '┤',
-	createDir(BoxDouble, BoxDouble, BoxSingle, BoxNone): '╣',
-	createDir(BoxDouble, BoxDouble, BoxSingle, BoxNone): '╢',
-	createDir(BoxSingle, BoxSingle, BoxDouble, BoxNone): '╡',
-	createDir(BoxHeavy, BoxHeavy, BoxHeavy, BoxNone):    '┫',
-	createDir(BoxSingle, BoxHeavy, BoxHeavy, BoxNone):   '┨',
-	createDir(BoxHeavy, BoxSingle, BoxSingle, BoxNone):  '┥',
-	createDir(BoxSingle, BoxHeavy, BoxHeavy, BoxNone):   '┪',
-	createDir(BoxHeavy, BoxSingle, BoxSingle, BoxNone):  '┦',
-	createDir(BoxHeavy, BoxSingle, BoxHeavy, BoxNone):   '┩',
-	createDir(BoxSingle, BoxHeavy, BoxSingle, BoxNone):  '┧',
+	BoxParts{BoxSingle, BoxSingle, BoxSingle, BoxNone}: '┤',
+	BoxParts{BoxDouble, BoxDouble, BoxSingle, BoxNone}: '╣',
+	BoxParts{BoxDouble, BoxDouble, BoxSingle, BoxNone}: '╢',
+	BoxParts{BoxSingle, BoxSingle, BoxDouble, BoxNone}: '╡',
+	BoxParts{BoxHeavy, BoxHeavy, BoxHeavy, BoxNone}:    '┫',
+	BoxParts{BoxSingle, BoxHeavy, BoxHeavy, BoxNone}:   '┨',
+	BoxParts{BoxHeavy, BoxSingle, BoxSingle, BoxNone}:  '┥',
+	BoxParts{BoxSingle, BoxHeavy, BoxHeavy, BoxNone}:   '┪',
+	BoxParts{BoxHeavy, BoxSingle, BoxSingle, BoxNone}:  '┦',
+	BoxParts{BoxHeavy, BoxSingle, BoxHeavy, BoxNone}:   '┩',
+	BoxParts{BoxSingle, BoxHeavy, BoxSingle, BoxNone}:  '┧',
 	// T-Right
-	createDir(BoxSingle, BoxSingle, BoxNone, BoxSingle): '├',
-	createDir(BoxDouble, BoxDouble, BoxNone, BoxDouble): '╠',
-	createDir(BoxDouble, BoxDouble, BoxNone, BoxSingle): '╟',
-	createDir(BoxSingle, BoxSingle, BoxNone, BoxDouble): '╞',
-	createDir(BoxHeavy, BoxHeavy, BoxNone, BoxHeavy):    '┣',
-	createDir(BoxHeavy, BoxHeavy, BoxNone, BoxSingle):   '┠',
-	createDir(BoxSingle, BoxSingle, BoxNone, BoxHeavy):  '┝',
-	createDir(BoxSingle, BoxHeavy, BoxNone, BoxHeavy):   '┢',
-	createDir(BoxHeavy, BoxSingle, BoxNone, BoxSingle):  '┞',
-	createDir(BoxHeavy, BoxSingle, BoxNone, BoxHeavy):   '┡',
-	createDir(BoxSingle, BoxHeavy, BoxNone, BoxSingle):  '┟',
+	BoxParts{BoxSingle, BoxSingle, BoxNone, BoxSingle}: '├',
+	BoxParts{BoxDouble, BoxDouble, BoxNone, BoxDouble}: '╠',
+	BoxParts{BoxDouble, BoxDouble, BoxNone, BoxSingle}: '╟',
+	BoxParts{BoxSingle, BoxSingle, BoxNone, BoxDouble}: '╞',
+	BoxParts{BoxHeavy, BoxHeavy, BoxNone, BoxHeavy}:    '┣',
+	BoxParts{BoxHeavy, BoxHeavy, BoxNone, BoxSingle}:   '┠',
+	BoxParts{BoxSingle, BoxSingle, BoxNone, BoxHeavy}:  '┝',
+	BoxParts{BoxSingle, BoxHeavy, BoxNone, BoxHeavy}:   '┢',
+	BoxParts{BoxHeavy, BoxSingle, BoxNone, BoxSingle}:  '┞',
+	BoxParts{BoxHeavy, BoxSingle, BoxNone, BoxHeavy}:   '┡',
+	BoxParts{BoxSingle, BoxHeavy, BoxNone, BoxSingle}:  '┟',
 	// Cross
-	createDir(BoxSingle, BoxSingle, BoxSingle, BoxSingle): '┼',
-	createDir(BoxDouble, BoxDouble, BoxDouble, BoxDouble): '╬',
-	createDir(BoxSingle, BoxSingle, BoxDouble, BoxDouble): '╪',
-	createDir(BoxDouble, BoxDouble, BoxSingle, BoxSingle): '╫',
-	createDir(BoxHeavy, BoxHeavy, BoxHeavy, BoxHeavy):     '╋',
-	createDir(BoxSingle, BoxSingle, BoxHeavy, BoxHeavy):   '┿',
-	createDir(BoxHeavy, BoxHeavy, BoxSingle, BoxSingle):   '╂',
-	createDir(BoxSingle, BoxHeavy, BoxHeavy, BoxHeavy):    '╈',
-	createDir(BoxHeavy, BoxSingle, BoxSingle, BoxSingle):  '╀',
-	createDir(BoxHeavy, BoxSingle, BoxHeavy, BoxHeavy):    '╇',
-	createDir(BoxSingle, BoxHeavy, BoxSingle, BoxSingle):  '╁',
-	createDir(BoxHeavy, BoxHeavy, BoxSingle, BoxHeavy):    '╊',
-	createDir(BoxSingle, BoxSingle, BoxHeavy, BoxSingle):  '┽',
-	createDir(BoxHeavy, BoxHeavy, BoxHeavy, BoxSingle):    '╉',
-	createDir(BoxSingle, BoxSingle, BoxSingle, BoxHeavy):  '┾',
-	createDir(BoxSingle, BoxHeavy, BoxSingle, BoxHeavy):   '╆',
-	createDir(BoxHeavy, BoxSingle, BoxHeavy, BoxSingle):   '╃',
-	createDir(BoxSingle, BoxHeavy, BoxHeavy, BoxSingle):   '╅',
-	createDir(BoxHeavy, BoxSingle, BoxSingle, BoxHeavy):   '╄',
+	BoxParts{BoxSingle, BoxSingle, BoxSingle, BoxSingle}: '┼',
+	BoxParts{BoxDouble, BoxDouble, BoxDouble, BoxDouble}: '╬',
+	BoxParts{BoxSingle, BoxSingle, BoxDouble, BoxDouble}: '╪',
+	BoxParts{BoxDouble, BoxDouble, BoxSingle, BoxSingle}: '╫',
+	BoxParts{BoxHeavy, BoxHeavy, BoxHeavy, BoxHeavy}:     '╋',
+	BoxParts{BoxSingle, BoxSingle, BoxHeavy, BoxHeavy}:   '┿',
+	BoxParts{BoxHeavy, BoxHeavy, BoxSingle, BoxSingle}:   '╂',
+	BoxParts{BoxSingle, BoxHeavy, BoxHeavy, BoxHeavy}:    '╈',
+	BoxParts{BoxHeavy, BoxSingle, BoxSingle, BoxSingle}:  '╀',
+	BoxParts{BoxHeavy, BoxSingle, BoxHeavy, BoxHeavy}:    '╇',
+	BoxParts{BoxSingle, BoxHeavy, BoxSingle, BoxSingle}:  '╁',
+	BoxParts{BoxHeavy, BoxHeavy, BoxSingle, BoxHeavy}:    '╊',
+	BoxParts{BoxSingle, BoxSingle, BoxHeavy, BoxSingle}:  '┽',
+	BoxParts{BoxHeavy, BoxHeavy, BoxHeavy, BoxSingle}:    '╉',
+	BoxParts{BoxSingle, BoxSingle, BoxSingle, BoxHeavy}:  '┾',
+	BoxParts{BoxSingle, BoxHeavy, BoxSingle, BoxHeavy}:   '╆',
+	BoxParts{BoxHeavy, BoxSingle, BoxHeavy, BoxSingle}:   '╃',
+	BoxParts{BoxSingle, BoxHeavy, BoxHeavy, BoxSingle}:   '╅',
+	BoxParts{BoxHeavy, BoxSingle, BoxSingle, BoxHeavy}:   '╄',
 }
